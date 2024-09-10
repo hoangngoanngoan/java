3704be648f1824bea65a12bd5aae5d7c3b27515a
hoangngoanngoan <ldh2381995@gmail.com>
2024-09-10 19:56:24 +0700

    Thêm bài 2 tính tổng hai số

---
+++ src/spc/edu/Bai2_TinhTong.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+/**
+ *
+ * @author SPC
+ */
+public class Bai2_TinhTong {
+    public static void main(String[] args) {
+        System.out.println("Chuong trinh tinh tong hai so nguyen");
+        int a = 10, b = 2;
+        int tong = a + b;
+        System.out.println(a + " + " + b + " = " + tong);
+    }
+}
